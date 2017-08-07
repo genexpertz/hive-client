386ef434e9b9a9db6d7a24e20f1a36c4ad8d0280
Gireesh Babu <gireesh.babu@nibodha.com>
2017-08-07 11:08:41 +0530

    Inital import

---
+++ src/main/java/com/expertzlab/hiveclient/TestJava.java
@@ -0,0 +1,11 @@
+package com.expertzlab.hiveclient;
+
+/**
+ * Created by gireeshbabu on 07/08/17.
+ */
+public class TestJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
